9171e42c1b78654e4cf770a481671c0b4de641fe
thenewmona <thenewmona@gmail.com>
2018-07-07 18:49:25 -0400

    added JS and jQuery  but it is still not working

--- js/designs.js
+++ js/designs.js
@@ -9,11 +9,30 @@ $("sizePicker").submit(function (event) {
   height = $("inputHeight").val();
   width = $("inputWeight").val()
   makeGrid(height, width);
+
   console.log("Height: " + height + " and width: " + width);
 })
 
-function makeGrid() {
+function makeGrid(x, y) {
+  $("tr").remove();
 
   // Your code goes here!
+  for (var i = 1; i <= x; i++) {
+    $("#pixelCanvas").append("<tr id=table" + i + "></tr>");
+    for (var i = 1; i <= x; i++) {
+      $("#table" + i).append("<td></td>");
+    }
+  }
+  //adding color to cell
+  $("td").click(function addColor() {
+    color = $("#colorPicker").val();
 
+    if ($(this).attr("style")) {
+      $(this).removeAttr("style")
+
+    }else {
+      $(this).attr("style", "background-color:" + color);
+    }
+
+  })
 }
