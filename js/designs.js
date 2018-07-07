9691feda86e22b5b300389adccc115f314301424
thenewmona <thenewmona@gmail.com>
2018-07-07 15:44:50 -0400

    added jQuery and makeGrid, can not render console

--- js/designs.js
+++ js/designs.js
@@ -1,7 +1,16 @@
 // Select color input
 // Select size input
+var height, width, color;
 
 // When size is submitted by the user, call makeGrid()
+
+$("sizePicker").submit(function (event) {
+  event.preventDefault();
+  height = $("inputHeight").val();
+  width = $("inputWeight").val()
+  makeGrid(height, width);
+  console.log("Height: " + height + " and width: " + width);
+})
 
 function makeGrid() {
 
